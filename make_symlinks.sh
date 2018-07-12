915a10b88f9799d7b04696ba7bdd8fdbe53ebb2f
Joseph Gambino <joe@gam.bingo>
2018-07-12 11:42:06 -0500

    symlink

---
+++ make_symlinks.sh
@@ -0,0 +1,2 @@
+# ln -s source_file myfile
+ln -s "/Users/jgambino/Dropbox (IDEO)/0 - Whitespace/4 - Cheesereel/projectcheesereel/audio" "/Users/jgambino/whitespace/5-interviews/audio_transcription"
